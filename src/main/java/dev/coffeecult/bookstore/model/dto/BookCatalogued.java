1bc0e8440029fc1326d9befb913a67a73d5902e4
SoyNegro <krisoul.valdes@gmail.com>
2022-10-03 10:55:22 -0400

    Domain Model BookCatalogued DTO for book listing

---
+++ src/main/java/dev/coffeecult/bookstore/model/dto/BookCatalogued.java
@@ -0,0 +1,12 @@
+package dev.coffeecult.bookstore.model.dto;
+
+import dev.coffeecult.bookstore.model.Book;
+import dev.coffeecult.bookstore.model.Rating;
+
+import java.time.LocalDateTime;
+
+public record BookCatalogued(String name, Double rating, int views, int chaptersCount, LocalDateTime lastUpdatedAt) {
+    public BookCatalogued(Book book){
+        this(book.title(),book.getRate(), book.views(), book.getChapterCount(),book.lastUpdatedAt());
+    }
+}
